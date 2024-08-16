4d44d49920e76730fb942815b941da910aeb8f66
Mangesh <bmangesh46@gmail.com>
2024-08-16 19:17:24 +0530

    Initial basic commit

---
+++ app/about/page.jsx
@@ -0,0 +1,4 @@
+const About = () => {
+  return <div className="bg-red-500">this is about me</div>;
+};
+export default About;
---
+++ components/navbar.jsx
@@ -0,0 +1,4 @@
+const Navbar = () => {
+  return <div className="bg-blue-300">this is navbar</div>;
+};
+export default Navbar;
